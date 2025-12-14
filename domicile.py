76bfe33f58992a3b5e29be418029598806b713a6
Muneebelze99 <muneebelze99@gmail.com>
2025-12-14 09:16:40 +0500

    Update domicile.py

--- domicile.py
+++ domicile.py
@@ -1,4 +1,9 @@
-def check_domicile(age,domicile):
-    print("yours")
-    
-    check_domicile()
+def check_domicile():
+  age=int(input("Enter your age: "))
+  domicile=input("Enter your domicile: ")
+
+  if domicile.lower()=="lahore" and age>=18:
+      print("You are eligible to vote.")
+  else:
+        print("You are not eligible to vote.")
+check_domicile()
